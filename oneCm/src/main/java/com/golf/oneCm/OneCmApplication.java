dc318e9f9c88a041dc1a6592ef5aea7d786086d5
shwjdgus1234 <shwjdgus12345@naver.com>
2024-12-07 15:07:27 +0900

    골프 회원 등록

---
+++ oneCm/src/main/java/com/golf/oneCm/OneCmApplication.java
@@ -0,0 +1,13 @@
+package com.golf.oneCm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneCmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneCmApplication.class, args);
+	}
+
+}
---
+++ oneCm/src/test/java/com/golf/oneCm/OneCmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.golf.oneCm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneCmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
